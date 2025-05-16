34303608cb02c37f7a031eb431589ac605ad77e5
neossta <nastyasw1@gmail.com>
2025-05-16 21:26:46 +0300

    add fonts

---
+++ client/src/assets/fonts/MetaPro/index.ts
@@ -0,0 +1,158 @@
+import MetaProBlackTtf from './MetaPro-Black.ttf';
+import MetaProBlackEot from './MetaPro-Black.eot';
+import MetaProBlackWoff from './MetaPro-Black.woff';
+import MetaProBlackWoff2 from './MetaPro-Black.woff2';
+
+import MetaProBoldTtf from './MetaPro-Bold.ttf';
+import MetaProBoldEot from './MetaPro-Bold.eot';
+import MetaProBoldWoff from './MetaPro-Bold.woff';
+import MetaProBoldWoff2 from './MetaPro-Bold.woff2';
+
+import MetaProBookTtf from './MetaPro-Book.ttf';
+import MetaProBookEot from './MetaPro-Book.eot';
+import MetaProBookWoff from './MetaPro-Book.woff';
+import MetaProBookWoff2 from './MetaPro-Book.woff2';
+
+import MetaProLightTtf from './MetaPro-Light.ttf';
+import MetaProLightEot from './MetaPro-Light.eot';
+import MetaProLightWoff from './MetaPro-Light.woff';
+import MetaProLightWoff2 from './MetaPro-Light.woff2';
+
+import MetaProMediumTtf from './MetaPro-Medi.ttf';
+import MetaProMediumEot from './MetaPro-Medi.eot';
+import MetaProMediumWoff from './MetaPro-Medi.woff';
+import MetaProMediumWoff2 from './MetaPro-Medi.woff2';
+
+import MetaProNormalTtf from './MetaPro-Norm.ttf';
+import MetaProNormalEot from './MetaPro-Norm.eot';
+import MetaProNormalWoff from './MetaPro-Norm.woff';
+import MetaProNormalWoff2 from './MetaPro-Norm.woff2';
+
+import MetaProCondBlackTtf from './MetaPro-CondBlack.ttf';
+import MetaProCondBlackEot from './MetaPro-CondBlack.eot';
+import MetaProCondBlackWoff from './MetaPro-CondBlack.woff';
+import MetaProCondBlackWoff2 from './MetaPro-CondBlack.woff2';
+
+import MetaProCondBoldTtf from './MetaPro-CondBold.ttf';
+import MetaProCondBoldEot from './MetaPro-CondBold.eot';
+import MetaProCondBoldWoff from './MetaPro-CondBold.woff';
+import MetaProCondBoldWoff2 from './MetaPro-CondBold.woff2';
+
+import MetaProCondBookTtf from './MetaPro-CondBook.ttf';
+import MetaProCondBookEot from './MetaPro-CondBook.eot';
+import MetaProCondBookWoff from './MetaPro-CondBook.woff';
+import MetaProCondBookWoff2 from './MetaPro-CondBook.woff2';
+
+import MetaProCondExtraBoldTtf from './MetaPro-CondXbold.ttf';
+import MetaProCondExtraBoldEot from './MetaPro-CondXbold.eot';
+import MetaProCondExtraBoldWoff from './MetaPro-CondXbold.woff';
+import MetaProCondExtraBoldWoff2 from './MetaPro-CondXbold.woff2';
+
+import MetaProCondMediumTtf from './MetaPro-CondMedi.ttf';
+import MetaProCondMediumEot from './MetaPro-CondMedi.eot';
+import MetaProCondMediumWoff from './MetaPro-CondMedi.woff';
+import MetaProCondMediumWoff2 from './MetaPro-CondMedi.woff2';
+
+import MetaProCondNormalTtf from './MetaPro-CondNorm.ttf';
+import MetaProCondNormalEot from './MetaPro-CondNorm.eot';
+import MetaProCondNormalWoff from './MetaPro-CondNorm.woff';
+import MetaProCondNormalWoff2 from './MetaPro-CondNorm.woff2';
+
+const Black = {
+  ttf: MetaProBlackTtf,
+  eot: MetaProBlackEot,
+  woff: MetaProBlackWoff,
+  woff2: MetaProBlackWoff2,
+};
+
+const Bold = {
+  ttf: MetaProBoldTtf,
+  eot: MetaProBoldEot,
+  woff: MetaProBoldWoff,
+  woff2: MetaProBoldWoff2,
+};
+
+const Book = {
+  ttf: MetaProBookTtf,
+  eot: MetaProBookEot,
+  woff: MetaProBookWoff,
+  woff2: MetaProBookWoff2,
+};
+
+const Light = {
+  ttf: MetaProLightTtf,
+  eot: MetaProLightEot,
+  woff: MetaProLightWoff,
+  woff2: MetaProLightWoff2,
+};
+
+const Medium = {
+  ttf: MetaProMediumTtf,
+  eot: MetaProMediumEot,
+  woff: MetaProMediumWoff,
+  woff2: MetaProMediumWoff2,
+};
+
+const Normal = {
+  ttf: MetaProNormalTtf,
+  eot: MetaProNormalEot,
+  woff: MetaProNormalWoff,
+  woff2: MetaProNormalWoff2,
+};
+
+const CondBlack = {
+  ttf: MetaProCondBlackTtf,
+  eot: MetaProCondBlackEot,
+  woff: MetaProCondBlackWoff,
+  woff2: MetaProCondBlackWoff2,
+};
+
+const CondBold = {
+  ttf: MetaProCondBoldTtf,
+  eot: MetaProCondBoldEot,
+  woff: MetaProCondBoldWoff,
+  woff2: MetaProCondBoldWoff2,
+};
+
+const CondBook = {
+  ttf: MetaProCondBookTtf,
+  eot: MetaProCondBookEot,
+  woff: MetaProCondBookWoff,
+  woff2: MetaProCondBookWoff2,
+};
+
+const CondExtraBold = {
+  ttf: MetaProCondExtraBoldTtf,
+  eot: MetaProCondExtraBoldEot,
+  woff: MetaProCondExtraBoldWoff,
+  woff2: MetaProCondExtraBoldWoff2,
+};
+
+const CondMedium = {
+  ttf: MetaProCondMediumTtf,
+  eot: MetaProCondMediumEot,
+  woff: MetaProCondMediumWoff,
+  woff2: MetaProCondMediumWoff2,
+};
+
+const CondNormal = {
+  ttf: MetaProCondNormalTtf,
+  eot: MetaProCondNormalEot,
+  woff: MetaProCondNormalWoff,
+  woff2: MetaProCondNormalWoff2,
+};
+
+export const MetaPro = {
+  Black,
+  Bold,
+  Book,
+  CondBlack,
+  CondBold,
+  CondBook,
+  CondExtraBold,
+  CondMedium,
+  CondNormal,
+  Light,
+  Medium,
+  Normal,
+};
