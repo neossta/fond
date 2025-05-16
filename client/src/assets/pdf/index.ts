b70bb4fa7932e53f0cc81e9cdcd95492e9c1293e
neossta <nastyasw1@gmail.com>
2025-05-16 21:35:48 +0300

    add pdf

---
+++ client/src/assets/pdf/index.ts
@@ -0,0 +1,5 @@
+export { default as RequisitesPDF } from './requisites.pdf';
+export { default as CertificatePDF } from './certificate.pdf';
+export { default as CharterPDF } from './charter.pdf';
+export { default as PositionPDF } from './position.pdf';
+export { default as AgreementPDF } from './agreement.pdf';
